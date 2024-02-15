76fd4044909e7812c93538a064aae0d8070f0a5d
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-15 12:54:44 +0700

    removed from index and added to reviews
    trying to get my test set up

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,25 @@ const router = Router()
 
 // Define a GET route for fetching the list of reviews
 router.get('/reviews', (req, res) => {
-  res.send('List of reviews')
+  const reviews = [
+    {
+      review_id: 1,
+      user_id: 2,
+      house_id: 34,
+      rating: 4.5,
+      comment: 'it was a great prooperty',
+      date: '2023-01-21'
+    },
+    {
+      review_id: 2,
+      user_id: 4,
+      house_id: 34,
+      rating: 4.0,
+      comment: 'Awesome location',
+      date: '2024-02-02'
+    }
+  ]
+  res.send(reviews)
 })
 
 // Define a GET route for fetching a single review
