653e8a49bc5dee1c78c7c94ce329a73127f3889b
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-15 12:27:21 +0700

    updated index

--- routes/reviews.js
+++
@@ -1,8 +0,0 @@
-app.get('/reviews', (req, res) => {
-    const reviews = [
-        { review_id: 1, user_id: 2, house_id: 34, rating: 4.5, comment: 'it was a great prooperty', date: '2023-01-21'},
-        { review_id: 2, user_id: 4, house_id: 34, rating: 4.0, comment: 'Awesome location', date: '2024-02-02'},
-        
-    ]
-    res.send(reviews)
-})
