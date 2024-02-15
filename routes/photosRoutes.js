60471dc662db565562c9a8bb1422dd7c8a7904cd
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 13:39:41 +0700

    create a variable for each router and store the data in it

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -2,16 +2,17 @@ import { Router } from 'express'
 const router = Router()
 //This the route that acces to all photos data"
 router.get('/photos', (req, res) => {
-  res.json([
+  const houses = [
     { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }
-  ])
+  ]
+  res.json(houses)
 })
 //This is the route that acces to each photo data"
 router.get('/photos/1', (req, res) => {
-  res.json({ photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' })
+  const house = { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' }
+  res.json(house)
 })
-
 export default router
