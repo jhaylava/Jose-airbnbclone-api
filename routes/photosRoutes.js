df62218cf800f0b79c0a39fbf1831e276d3c978e
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 13:46:40 +0700

    change the name of the variables for each router

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,18 +1,18 @@
 import { Router } from 'express'
 const router = Router()
-//This the route that acces to all photos data"
+//This is the route that acces to all photos data"
 router.get('/photos', (req, res) => {
-  const houses = [
+  const photos = [
     { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }
   ]
-  res.json(houses)
+  res.json(photos)
 })
 //This is the route that acces to each photo data"
 router.get('/photos/1', (req, res) => {
-  const house = { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' }
-  res.json(house)
+  const photo = { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' }
+  res.json(photo)
 })
 export default router
