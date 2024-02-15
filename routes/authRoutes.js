c1966c2445f4e5219a139db5361d4af678f214dd
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-15 13:51:46 +0700

    removed 1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,21 +5,21 @@ router.get('/signup', (req, res) => {
   res.send('List of signups')
 })
 
-router.get('/signup/1', (req, res) => {
+router.get('/signup/', (req, res) => {
   res.send('Hello from SignUp')
 })
 
 router.get('/login', (req, res) => {
   res.send('List of login')
 })
-router.get('/login/1', (req, res) => {
+router.get('/login/', (req, res) => {
   res.send('Hello from Login')
 })
 
 router.get('/logout', (req, res) => {
   res.send('List of logout')
 })
-router.get('/login/1', (req, res) => {
+router.get('/login/', (req, res) => {
   res.send('Hello from Logout')
 })
 
