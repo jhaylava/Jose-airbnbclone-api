257fc2ef0585c6ebb7ac610dda90976d1e0f59ac
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-15 13:40:29 +0700

    created route for login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,11 @@ router.get('/signup/1', (req, res) => {
   res.send('Hello from SignUp')
 })
 
+router.get('/login', (req, res) => {
+  res.send('List of login')
+})
+router.get('/login/1', (req, res) => {
+  res.send('Hello from Login')
+})
+
 export default router
