19e8fbe1828457c43904a5358bfb9f2a3fc45ead
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 22:41:44 +0700

    rename the imported router and improve the comments on photosRoutes file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 
 const app = express()
-import photosRouter from './routes/photosRoutes.js'
+import photosRoutes from './routes/photosRoutes.js'
 
-app.use(photosRouter)
+app.use(photosRoutes)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,18 +1,22 @@
 import { Router } from 'express'
 const router = Router()
-//This is the route that acces to all photos data"
+// Route to access all photos data
 router.get('/photos', (req, res) => {
+  // Sample data for photos
   const photos = [
     { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
     { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }
   ]
+  // Send the photos data as JSON response
   res.json(photos)
 })
-//This is the route that acces to each photo data"
+// Route to access data of a specific photo (photo_id = 1)
 router.get('/photos/1', (req, res) => {
+  // Sample data for a specific photo
   const photo = { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' }
+  // Send the specific photo data as JSON response
   res.json(photo)
 })
 export default router
