0b2b300d39a3a3130666c9921e4c414608c2b644
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-15 12:29:25 +0700

    added get send to reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,2 +1,7 @@
 import { Router } from 'express'
 const router = Router()
+
+// Define a GET route for fetching the list of reviews
+router.get('/reviews', (req, res) => {
+  res.send('List of reviews')
+})
