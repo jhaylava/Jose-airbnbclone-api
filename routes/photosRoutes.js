25e3953f318ae1bfa3618beaa7caba35aebfb052
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 13:27:57 +0700

    replace .send by .json and comment each router, the typos are fixed

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,15 +1,17 @@
 import { Router } from 'express'
-cons router = Router()
-//Seting the route to 
+const router = Router()
+//This the route that acces to all photos data"
 router.get('/photos', (req, res) => {
-    res.send([{ photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
-        { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
-        { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
-        { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }])
+  res.json([
+    { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
+    { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
+    { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
+    { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }
+  ])
 })
-
+//This is the route that acces to each photo data"
 router.get('/photos/1', (req, res) => {
-    res.send({{photo_id: 1,photo1: 'url',photo2:'url',photo3:'url'}})
+  res.json({ photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' })
 })
 
 export default router
