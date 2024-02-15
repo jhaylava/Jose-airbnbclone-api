f4240af28aeb4b2d485cb8fc74b9bbed2830f189
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 12:43:37 +0700

    add more objects to the array of houses and create a route for each house details

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -8,9 +8,33 @@ router.get('/houses', (req, res) => {
       location: 'Bangkok',
       price: 70,
       reviews: 'bunch of text',
-      host: 2
+      host: 'Lisa'
+    },
+    {
+      house_id: 2,
+      location: 'Chiang Mai',
+      price: 40,
+      reviews: 'bunch of text and more',
+      host: 'Nam'
+    },
+    {
+      house_id: 3,
+      location: 'Phuket',
+      price: 150,
+      reviews: 'bunch of text, clubs, food and crazy mess',
+      host: 'Smirnoff'
     }
   ])
+})
+
+router.get('/houses/1', (req, res) => {
+  res.send({
+    house_id: 1,
+    location: 'Bangkok',
+    price: 70,
+    reviews: 'bunch of text',
+    host: 'Lisa'
+  })
 })
 
 export default router
