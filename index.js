f4d3a6216a9d0254b202b2ac355071208cf80080
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 13:06:56 +0700

    router from photos imported and ready to use

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 
 const app = express()
+import { photosRouter } from './routes/photosRoutes.js'
+
+app.use(photosRouter)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
