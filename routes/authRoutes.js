8803211ac5c423d7d347ac39be6c8ce76e705e62
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-15 13:39:07 +0700

    fixed list of users to list of Signups

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/signup', (req, res) => {
-  res.send('List of users')
+  res.send('List of signups')
 })
 
 router.get('/signup/1', (req, res) => {
   res.send('Hello from SignUp')
 })
+
 export default router
