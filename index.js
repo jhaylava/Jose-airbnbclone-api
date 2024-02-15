f3fd95c92457d023b7c95fe40dd97e0a07972ea5
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-15 13:52:00 +0700

    imported authRoutes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 
 const app = express()
-
+import authRoutes from './routes/authRoutes.js'
+app.use(authRoutes)
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
 })
