d9b903a8d83827d680af780955ca6165017b1e9e
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-15 22:33:43 +0700

    rename variable imported to housesRouters

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const app = express()
 
-import housesRouter from './routes/housesRoutes.js'
+import housesRouters from './routes/housesRoutes.js'
 
-app.use(housesRouter)
+app.use(housesRouters)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
