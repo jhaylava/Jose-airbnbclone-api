711ee91d506d94122c37c344d0c608043851b12b
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-16 13:26:36 +0700

    importe the data from data base

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,51 +1,28 @@
 import { Router } from 'express'
+import db from '../db.js'
 const router = Router()
 
 // Define a GET route for fetching the list of reviews
-router.get('/bookings', (req, res) => {
-  const bookings = [
-    {
-      booking_id: 1,
-      house_id: 2,
-      guest_id: 1,
-      host_id: 34,
-      date_start: '2024-02-20',
-      date_end: '2024-02-22',
-      total_nights: 2,
-      price_daily: 24,
-      price_total: 48,
-      message: 'Cant wait to explore your island'
-    },
-    {
-      booking_id: 2,
-      house_id: 2,
-      guest_id: 3,
-      host_id: 34,
-      date_start: '2024-03-01',
-      date_end: '2024-03-05',
-      total_nights: 4,
-      price_daily: 24,
-      price_total: 96,
-      message: 'Looking forward to the stay'
-    }
-  ]
-  res.json(bookings)
+router.get('/bookings', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM bookings')
+    res.json(result)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
 })
 
 // Define a GET route for fetching a single review
-router.get('/bookings/1', (req, res) => {
-  res.json({
-    booking_id: 1,
-    house_id: 2,
-    guest_id: 1,
-    host_id: 34,
-    date_start: '2024-02-20',
-    date_end: '2024-02-22',
-    total_nights: 2,
-    price_daily: 24,
-    price_total: 48,
-    message: 'Cant wait to explore your island'
-  })
+router.get('/bookings/1', async (req, res) => {
+  try {
+    const query = await db.query(
+      'SELECT * FROM bookings WHERE bookings.booking_id = 1'
+    )
+    const bookingsArray = query.rows
+    res.json(bookingsArray[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
 })
 
 // Export the router
