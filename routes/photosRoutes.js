79a76f9e77b225026b24d01096d69230e6032bf8
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-16 13:39:42 +0700

    photos route respond to specific photos from databse

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import db from '../db.js'
 const router = Router()
 // Route to access all photos data
-router.get('/users', async (req, res) => {
+router.get('/photos', async (req, res) => {
   try {
     const { rows } = await db.query('SELECT * FROM photos') // query the database
     console.log(rows)
@@ -14,10 +14,16 @@ router.get('/users', async (req, res) => {
 })
 
 // Route to access data of a specific photo (photo_id = 1)
-router.get('/photos/1', (req, res) => {
-  // Sample data for a specific photo
-  const photo = { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' }
-  // Send the specific photo data as JSON response
-  res.json(photo)
+router.get('/photos/1', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM photos WHERE photo_id = 1')
+    console.log(rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    // send specific photo data as JSON response
+    res.json(err)
+  }
 })
+
 export default router
