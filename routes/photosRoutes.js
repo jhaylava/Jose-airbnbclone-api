3edefde4a6436fa616de0c6adae69f704a1d4945
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-16 13:34:04 +0700

    photos route to read all photos and respond with array of photos from database

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -2,17 +2,17 @@ import { Router } from 'express'
 import db from '../db.js'
 const router = Router()
 // Route to access all photos data
-router.get('/photos', (req, res) => {
-  // Sample data for photos
-  const photos = [
-    { photo_id: 1, photo1: 'url', photo2: 'url', photo3: 'url' },
-    { photo_id: 2, photo1: 'url', photo2: 'url', photo3: 'url' },
-    { photo_id: 3, photo1: 'url', photo2: 'url', photo3: 'url' },
-    { photo_id: 4, photo1: 'url', photo2: 'url', photo3: 'url' }
-  ]
-  // Send the photos data as JSON response
-  res.json(photos)
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM photos') // query the database
+    console.log(rows)
+    res.json(rows) // respond with the data
+  } catch (err) {
+    console.error(err.message)
+    res.json(err)
+  }
 })
+
 // Route to access data of a specific photo (photo_id = 1)
 router.get('/photos/1', (req, res) => {
   // Sample data for a specific photo
