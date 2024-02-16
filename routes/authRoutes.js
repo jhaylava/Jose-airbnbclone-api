4ea02773b87de1fbc5245b2c4f43c25c191393ab
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-16 08:46:08 +0700

    changed to only 3 routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,24 +2,14 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/signup', (req, res) => {
-  res.send('List of signups')
-})
-
-router.get('/signup/', (req, res) => {
-  res.send('Hello from SignUp')
+  res.send('Hello from Signup')
 })
 
 router.get('/login', (req, res) => {
-  res.send('List of login')
-})
-router.get('/login/', (req, res) => {
   res.send('Hello from Login')
 })
 
 router.get('/logout', (req, res) => {
-  res.send('List of logout')
-})
-router.get('/login/', (req, res) => {
   res.send('Hello from Logout')
 })
 
