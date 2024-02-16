1afc2a38978eb1eb1c51c3f6ef2b8170c84ab264
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-16 08:43:08 +0700

    renamed userRoutes to usersRoutes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 
 const app = express()
-import userRoutes from './routes/userRoutes.js'
+import usersRoutes from './routes/userRoutes.js'
 
 // Tell the app to use the user router
-app.use(userRoutes)
+app.use(usersRoutes)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
