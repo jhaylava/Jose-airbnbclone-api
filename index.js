80db86ae0fd6fa021f8ffb83a643a48571b81dd4
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-16 08:41:31 +0700

    changed to routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 const app = express()
 
-import bookingsRouter from './routes/bookings.js'
-app.use(bookingsRouter)
+import bookingsRoutes from './routes/bookings.js'
+app.use(bookingsRoutes)
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
 })
