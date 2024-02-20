552d04bc5928d51473b735751ca0de5b0bcb88c1
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-20 11:23:43 +0700

    imported db and put all imports together

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import db from '../db.js'
 const router = Router()
 
-router.get('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
   res.send('Hello from Signup')
 })
 
