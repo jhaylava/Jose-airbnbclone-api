8e4e504cb2fcfc6fba2912e72c352e5f3e69547e
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-21 12:55:11 +0700

    create a router to let the client delete any data related to houses

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -64,5 +64,15 @@ router.get('/houses/:house_id', async (req, res) => {
     res.json({ error: err.message })
   }
 })
+router.delete('/houses/:house_id', async (req, res) => {
+  try {
+    const { countRows } = db.query(
+      `DELETE FROM houses WHERE house_id = ${req.params.house_id}`
+    )
+    res.json(countRows)
+  } catch (error) {
+    res.json({ error: err.message })
+  }
+})
 
 export default router
