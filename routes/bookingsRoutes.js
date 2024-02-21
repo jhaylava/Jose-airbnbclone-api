65f9d15bc154c93cb924d22345f80b6e9a3ba400
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-02-21 10:53:15 +0700

    removed console.log since everything works now

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -10,15 +10,6 @@ router.post('/bookings', async (req, res) => {
   const arrival_date = req.body.arrival_date
   const departure_date = req.body.departure_date
   const comment = req.body.comment
-  console.log(
-    req.body,
-    house_id,
-    user_id,
-    price,
-    arrival_date,
-    departure_date,
-    comment
-  )
   const queryString = `
     INSERT INTO bookings ( house_id, user_id, price, arrival_date, departure_date, comment)
     VALUES (${house_id}, ${user_id}, ${price},'${arrival_date}', '${departure_date}', '${comment}')
